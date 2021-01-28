37671bfa1c7b17e8b55f3036443d150ac6e1808f
Zack Werner <allthelight044@gmail.com>
2021-01-28 14:06:13 -0600

    getters and setters

--- client.js
+++ client.js
@@ -10,7 +10,15 @@ function onReady() {
 
   const h2 = $('h2');
   // make it blue
-  h2.css('background-color', 'blue');
+  // h2.css('background-color', 'blue');
   // This does the same thing below.
-  $(h2).css('background-color', 'green');
+  // $(h2).css('background-color', 'green');
+  $('h2').addClass('froggy');
+
+  // change the text
+  const h1Text = $(h1).text();
+  console.log('h1Text is ', h1Text);
+
+  const louderText = h1Text.toUpperCase() + '!!!!';
+  $('h1').text(louderText);
 }
