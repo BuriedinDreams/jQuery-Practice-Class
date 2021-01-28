56bfaf17c19b2420969f52864aa6c9fea411fa67
Zack Werner <allthelight044@gmail.com>
2021-01-28 13:47:34 -0600

    changing colors using jQuery

--- client.js
+++ client.js
@@ -4,4 +4,13 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log('jQuery up and running');
+
+  const h1 = $('h1');
+  console.log('h1', h1);
+
+  const h2 = $('h2');
+  // make it blue
+  h2.css('background-color', 'blue');
+  // This does the same thing below.
+  $(h2).css('background-color', 'green');
 }
